02480866ecbcec207d57ccd94f48257450964752
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-11-06 00:14:14 +0000

    r and c points

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -3,12 +3,12 @@ module Roadmap
 def get_roadmap(roadmap_id)
 
   response = self.class.get(@api_url + '/roadmaps/' + (roadmap_id.to_s), headers: { "authorization" => @auth_token })
-  body = JSON.parse(response.body)
+  JSON.parse(response.body)
 end
 
 def get_checkpoint(checkpoint_id)
   response = self.class.get(@api_url + '/checkpoints/' + (checkpoint_id.to_s), headers: { "authorization" => @auth_token })
-  body = JSON.parse(response.body)
+   JSON.parse(response.body)
 end
 
 end
